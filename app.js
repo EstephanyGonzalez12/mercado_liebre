e56b17109b178dd356508ec69108fc3f5def37d7
EstephanyGonzalez12 <estefanygonzalez999@gmail.com>
2022-06-23 15:58:40 -0500

    Agregué registro y login con su respectivo css

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
 });
 
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+
 app.listen(PORT, () => console.log('http://localhost:'+ PORT));
 
